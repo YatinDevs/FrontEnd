2c850e5799dfa53844739d631433cfcbd72fc6f5
Yatin Chaudhari <c.yatin727@gmail.com>
2023-08-31 15:43:25 +0530

    Add files via upload

---
+++ JavaScript(FrontEnd)/JavaScript/7_Truthy_Falsy_Js/if_else.js
@@ -0,0 +1,19 @@
+// If else condition
+
+let age = 17;
+
+if(age >= 18){
+   console.log("User can play ddlc");
+}else{
+    console.log("User can play mario");
+
+}
+
+let num = 13;
+
+if(num % 2==0){
+    console.log("even");
+}else{
+    console.log("odd");
+
+}
---
+++ JavaScript(FrontEnd)/JavaScript/7_Truthy_Falsy_Js/truthy_falsy.js
@@ -0,0 +1,26 @@
+// ################## Truthy and falsy values ######################
+
+
+// #1. falsy values
+
+// false
+// ""
+// null
+// undefined
+// 0
+
+// 2# .truthy values
+// all are truthy
+// "abc"
+// 1, -1
+
+
+// try all falsy values and truthy to see output
+
+let firstname;
+if(firstname){
+    console.log(firstName);
+}else{
+    console.log("empty variable");
+
+}

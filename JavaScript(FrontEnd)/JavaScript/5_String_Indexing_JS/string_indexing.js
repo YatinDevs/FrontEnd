68f8f4b8006fe9bfee64e0bfbd17c707a5180a2b
Yatin Chaudhari <c.yatin727@gmail.com>
2023-08-31 14:25:38 +0530

    Add files via upload

---
+++ JavaScript(FrontEnd)/JavaScript/5_String_Indexing_JS/string_indexing.js
@@ -0,0 +1,21 @@
+// console.log("hello World!!!");
+
+// #################### String Indexing ################
+
+let str = "Yatin";
+
+// Y a t i n
+// 0 1 2 3 4
+
+console.log(str[0]);
+console.log(str[1]);
+console.log(str[2]);
+console.log(str[3]);
+console.log(str[4]);
+
+// length of String
+
+console.log(str.length);
+
+// last Index :
+console.log(str[str.length-1]);

6562931fdfbb5e2df6c836ee8df6240be9f21201
Yatin Chaudhari <c.yatin727@gmail.com>
2023-09-05 18:07:48 +0530

    Add files via upload

---
+++ JavaScript(FrontEnd)/JavaScript/19_How_JS_Works/exercise.js
@@ -0,0 +1,14 @@
+
+// Analayse code :
+
+console.log("hello world");
+let first_name = "Yatin";
+let last_name = "Chaudhari";
+
+const myfunction = function (){
+    let var1 = "first var";
+    let var2 = "Second var";
+    console.log(var1);
+    console.log(var2);
+
+}

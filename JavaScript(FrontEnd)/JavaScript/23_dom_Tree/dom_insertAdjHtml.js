ba985abcdd193dbcf446cc8e94412ed2fd934cfe
Yatin Chaudhari <c.yatin727@gmail.com>
2023-09-09 11:46:02 +0530

    Add files via upload

---
+++ JavaScript(FrontEnd)/JavaScript/23_dom_Tree/dom_insertAdjHtml.js
@@ -0,0 +1,18 @@
+// elem.insertAdjacentHTML(where,html) --> Used Less/Method
+
+// beforebegin
+// afterbegin
+// beforend
+// // afterend
+
+// const todolist = document.querySelector(".todo-list");
+// todolist.insertAdjacentHTML("beforeend","<li>beforeEnd</li>");
+
+// const todolist = document.querySelector(".todo-list");
+// todolist.insertAdjacentHTML("afterbegin","<li>afterBegin</li>");
+
+// const todolist = document.querySelector(".todo-list");
+// todolist.insertAdjacentHTML("beforebegin","<li>beforebegin</li>");
+
+// const todolist = document.querySelector(".todo-list");
+// todolist.insertAdjacentHTML("afterend","<li>afterend</li>");

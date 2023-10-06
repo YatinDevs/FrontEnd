7621b7d6230969344e3c13ae98f9f0ff77520565
Yatin Chaudhari <c.yatin727@gmail.com>
2023-10-06 23:36:16 +0530

    Add files via upload

---
+++ JavaScript(FrontEnd)/JavaScript/Assignments/executionContext.js
@@ -0,0 +1,66 @@
+// var num = 10;
+
+// function x() {
+
+//     function y() {
+
+//         function z() {
+
+//             console.log(num);
+
+//         }
+//         z()
+//     }
+
+//     y()
+// }
+
+// x()
+
+// function x() {
+
+//     function y() {
+
+//         function z() {
+
+//             var num = 10;
+
+//         }
+//         z()
+//     }
+
+//     y()
+// }
+
+// x()
+// console.log(num);
+
+// var num = 3;
+
+// function multiplyBy2(inputNum) {
+
+//     var result = inputNum * 2;
+
+//     return result;
+
+// };
+
+// var output = multiplyBy2(num);
+
+// var newOutput = multiplyBy2(10);
+
+// console.log(output);
+
+// console.log(newOutput);
+
+// var num = 100;
+
+// var message = 'Hello!';
+
+// function x(msg) {
+//     var num = 200;
+//     console.log(msg);
+//     return num;
+// };
+// console.log(num);
+// console.log(x(message));

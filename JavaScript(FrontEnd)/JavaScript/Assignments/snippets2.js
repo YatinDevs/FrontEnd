d4eb5485bf8611ec2e36bdbbdc1362523481ac77
Yatin Chaudhari <c.yatin727@gmail.com>
2023-10-06 23:38:03 +0530

    Add files via upload

---
+++ JavaScript(FrontEnd)/JavaScript/Assignments/snippets2.js
@@ -0,0 +1,13 @@
+var num = 3;
+
+function multiplyBy2(inputNum) {
+  var result = inputNum * 2;
+  return result;
+}
+
+var output = multiplyBy2(num);
+
+var newOutput = multiplyBy2(10);
+
+console.log(output);
+console.log(newOutput);

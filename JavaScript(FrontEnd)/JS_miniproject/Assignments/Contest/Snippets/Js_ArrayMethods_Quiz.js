25ba2844976c04562af07ade84a3f0101544421a
Yatin Chaudhari <c.yatin727@gmail.com>
2023-10-09 13:29:59 +0530

    Add files via upload

---
+++ JavaScript(FrontEnd)/JS_miniproject/Assignments/Contest/Snippets/Js_ArrayMethods_Quiz.js
@@ -0,0 +1,51 @@
+/*
+const ele = ["1", "2", "3", "4"];
+let text = ele.join(",");
+console.log(text);
+
+const array1 = [1, 2, 3];
+const Element = array1.shift();
+console.log(Element);
+var arr = [34, 234, 567, 4];
+arr.push("jacob", true, 23.45);
+console.log(arr);
+
+const dogs = ["Fido", "Odie", "Oscar"];
+dogs.length = 1;
+console.log(dogs);
+
+let arr1 = [1, 3, 4, 5, 6];
+let arr2 = [7, 8, 9, 0];
+
+let arr3 = arr2.concat(arr1, array1);
+console.log(arr3);
+*/
+/*
+The_______ method returns the index (position) of the last occurrence of a specified value in a string.
+A
+lastIndex()
+B
+lastIndexOf()
+C
+lastOf()
+D
+IndexOf()
+
+ans  B
+lastIndexOf()
+*/
+
+const array1 = ["a", "b", "c"];
+const array2 = ["d", "e", "f"];
+const newArray = array1.concat(array2);
+
+console.log(newArray);
+const array3 = ["123", "456", "789"];
+console.log(array3.reverse());
+
+var a = "Topzenith";
+var result = a.substring(4, 5);
+document.write(result);
+
+var mathVal = Math.floor("f");
+console.log(mathVal);

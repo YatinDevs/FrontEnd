8a6964e392ec13f5ea96b511718f32ecb636003c
Yatin Chaudhari <c.yatin727@gmail.com>
2023-11-23 23:54:23 +0530

    Add files via upload

--- JavaScript(FrontEnd)/JavaScript/Assignments/Revision-AyushK/Day3/homework.js
+++ JavaScript(FrontEnd)/JavaScript/Assignments/Revision-AyushK/Day3/homework.js
@@ -37,25 +37,27 @@ const actors = [
 
 // 1. Print Names of those actors who are above 44
 // solution :
-actors.map((actor) => {
-  if (actor.age > 44) {
-    console.log(
-      "Guys with age above 44",
-      actor.firstName + " " + actor.lastName
-    );
-  }
-});
+// actors.map((actor) => {
+//   if (actor.age > 44) {
+//     console.log(
+//       "Guys with age above 44",
+//       actor.firstName + " " + actor.lastName
+//     );
+//   }
+// });
 
 // 2. Find Sum of age of those actors who are female using Reduce
-const SumofAge = actors.reduce((acc, currvalue) => {
-  console.log(currvalue);
-  // console.log(acc);
-  return;
-});
+// reduce((acc, currvalue, currentidx, arr) => {}, initialvalue);
+const SumofAge = actors.reduce((age, actor) => {
+  // console.log(actor.age);
+  return age + actor.age;
+}, 0);
+
+console.log(SumofAge);
 
 // 3. Print Names of those actors who are male
-actors.filter((actor) => {
-  if (actor.gender !== "Female") {
-    console.log(`Gender : Male , Name : ${actor.firstName} ${actor.lastName}`);
-  }
-});
+// actors.filter((actor) => {
+//   if (actor.gender !== "Female") {
+//     console.log(`Gender : Male , Name : ${actor.firstName} ${actor.lastName}`);
+//   }
+// });
